9c9274179789a0bde87ed47be6a581af11a7ee17
204 <204@204equipo18.informatica.usbbog.edu.co>
2018-05-03 15:37:41 -0500

    eje

---
+++ ej 1/src/ej/pkg1/Ej1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej.pkg1;
+
+/**
+ *
+ * @author 204
+ */
+public class Ej1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
