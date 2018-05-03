a09c851a7b89f9a9c83cb6f89ab8a20d04b88387
204 <204@204equipo27.informatica.usbbog.edu.co>
2018-05-03 15:59:35 -0500

    hiii

--- ej 1/src/ej/pkg1/Ej1.java
+++ ej 1/src/ej/pkg1/Ej1.java
@@ -15,7 +15,6 @@ public class Ej1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        System.out.println("hiiiiii");
     
 }
